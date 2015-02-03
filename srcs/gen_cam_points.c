1afac474894398ed809264a64f5097af15614985
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2015-02-03 12:52:23 +0100

    Norming

--- srcs/gen_cam_points.c
+++ srcs/gen_cam_points.c
@@ -6,7 +6,7 @@
 /*   By: tgauvrit <tgauvrit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/27 12:51:50 by tgauvrit          #+#    #+#             */
-/*   Updated: 2015/01/31 19:00:40 by tgauvrit         ###   ########.fr       */
+/*   Updated: 2015/02/03 12:04:55 by tgauvrit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,14 @@ void	gen_cam_points(t_wolf_cam *cam, t_wolf_wall *wall)
 	wall->right->cam_x = wall->right->x - cam->point->x;
 	wall->right->cam_z = wall->right->z - cam->point->z;
 	temp = wall->right->cam_z;
-	wall->right->cam_z = (wall->right->cam_z * cos(cam->y_ang * DEGREE)) - (wall->right->cam_x * sin(cam->y_ang * DEGREE));
-	wall->right->cam_x = (wall->right->cam_x * cos(cam->y_ang * DEGREE)) + (temp * sin(cam->y_ang * DEGREE));
+	wall->right->cam_z = (wall->right->cam_z * cos(cam->y_ang * DEGREE))
+		- (wall->right->cam_x * sin(cam->y_ang * DEGREE));
+	wall->right->cam_x = (wall->right->cam_x * cos(cam->y_ang * DEGREE))
+		+ (temp * sin(cam->y_ang * DEGREE));
 	temp = wall->left->cam_z;
-	wall->left->cam_z = (wall->left->cam_z * cos(cam->y_ang * DEGREE)) - (wall->left->cam_x * sin(cam->y_ang * DEGREE));
-	wall->left->cam_x = (wall->left->cam_x * cos(cam->y_ang * DEGREE)) + (temp * sin(cam->y_ang * DEGREE));
+	wall->left->cam_z = (wall->left->cam_z * cos(cam->y_ang * DEGREE))
+		- (wall->left->cam_x * sin(cam->y_ang * DEGREE));
+	wall->left->cam_x = (wall->left->cam_x * cos(cam->y_ang * DEGREE))
+		+ (temp * sin(cam->y_ang * DEGREE));
 	gen_cam_points(cam, wall->next);
 }
